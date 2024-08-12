9add254c4686dc3acef43608f00cb3feecfcf381
anshul9809 <kumar.anshul9809@gmail.com>
2024-08-12 14:11:09 +0530

    Changed the MongoDb connected phase

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console,"Error connecting to MongoDB ......"));
 
 db.once('open',function(){
-    console.log('Connected to Database :: Mongodb')
+    console.log("db connected");
 });
 
 module.exports = db;
